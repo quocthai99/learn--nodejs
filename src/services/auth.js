a7194b052c0e698ae03ecb3ee09dc43b5a7cbe3a
quocthai99 <quocthai5101999@gmail.comn>
2023-01-26 21:46:59 +0700

    login

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,6 @@
 import db from '../models'
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 const hashPassword = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 
@@ -12,10 +13,30 @@ export const register = ({email, password}) => new Promise(async(resolve, reject
                 password: hashPassword(password)
             }
         })
-
+        const token = response[1] ? jwt.sign({id: response[0].id, email: response[0].email, role_code: response[0].role_code}, process.env.JWT_SECRET, { expiresIn: '5d' }) : null;
         resolve({
             err: response[1] ? 0 : 1,
-            mes: response[1] ? "Register is successfully" : "Email is used"
+            mes: response[1] ? "Register is successfully" : "Email is used",
+            'access_token': token ? `Bearer ${token}` : token
+        })
+        
+    } catch (error) {
+        reject(error)
+    }
+})
+
+export const login = ({email, password}) => new Promise(async(resolve, reject) => {
+    try {
+        const response = await db.User.findOne({
+            where: { email },
+            raw: true
+        })
+        const isChecked = response && bcrypt.compareSync(password, response.password);
+        const token = isChecked ? jwt.sign({id: response.id, email: response.email, role_code: response.role_code}, process.env.JWT_SECRET, { expiresIn: '5d' })  : null;
+        resolve({
+            err: token ? 0 : 1,
+            mes: token ? "Login is successfully" : response ? 'password is wrong' : 'Email has been registered',
+            'access_token': token ? `Bearer ${token}` : token
         })
         
     } catch (error) {
