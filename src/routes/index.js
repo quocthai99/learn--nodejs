06aab70a2e9e8dfebe1345bf9da570f59e688c20
quocthai99 <quocthai5101999@gmail.comn>
2023-01-27 15:26:24 +0700

    done login register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 import user from './user'
 import auth from './auth'
+import {notFound} from '../middlewares/handle_errors'
 
 const initRoutes = (app) => {
 
     app.use('/api/v1/user', user)
     app.use('/api/v1/auth', auth)
 
-    return app.use('/', (req, res) => {
-        return res.send('SERVER ONNN ALRIGHT')
-    })
+    app.use(notFound)
 }
 
 module.exports = initRoutes
