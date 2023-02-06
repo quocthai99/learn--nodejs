5f1b58c8686d7d083a0b9a179ee57b4acaee02bc
quocthai99 <quocthai5101999@gmail.comn>
2023-02-07 00:11:48 +0700

    insert data

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,10 @@
 import  * as controllers from "../controllers"
 import express from "express"
 import verifyToken from "../middlewares/verify_token"
-import { isAdmin, isModeratorOrAdmin } from "../middlewares/verify_roles"
 
 const router = express.Router()
 
 router.use(verifyToken)
-router.use(isModeratorOrAdmin)
 router.get('/', controllers.getCurrent)
 
 module.exports = router
--- src/services/user.js
+++ src/services/user.js
@@ -1,20 +1,21 @@
-import db from "../models"
+import db from "../models";
 
-export const getOne = (userId) => new Promise(async(resolve, reject) => {
+export const getOne = (userId) =>
+  new Promise(async (resolve, reject) => {
     try {
-        const response = await db.User.findOne({
-            where: { id: userId },
-            attributes: {
-                exclude: ['password']
-            }
-        })
-        resolve({
-            err: response ? 0 : 1,
-            mes: response ? "Got" : 'User not found',
-            userData: response
-        })
-        
+      const response = await db.User.findOne({
+        where: { id: userId },
+        attributes: {
+          exclude: ["password", "role_code"],
+        },
+        include: [{ model: db.Role, as: "roleData", attributes: ['id', 'code', 'value'] }],
+      });
+      resolve({
+        err: response ? 0 : 1,
+        mes: response ? "Got" : "User not found",
+        userData: response,
+      });
     } catch (error) {
-        reject(error)
+      reject(error);
     }
-})
+  });
