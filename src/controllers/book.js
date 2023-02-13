5662c18e26631aca3be1680b598e63fbe8749976
quocthai99 <quocthai5101999@gmail.comn>
2023-02-13 12:51:59 +0700

    delete

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,6 +1,6 @@
 import * as services from "../services";
 import { badRequest, internalServerError } from "../middlewares/handle_errors";
-import {title, image, available, price, category_code} from "../helpers/joi_schema"
+import {title, image, available, price, category_code, bid, bids} from "../helpers/joi_schema"
 import joi from 'joi'
 const cloudinary = require('cloudinary').v2
 
@@ -25,6 +25,40 @@ export const createNewBook = async (req, res) => {
     }
 
     const response = await services.createNewBook(req.body, fileData);
+    return res.status(200).json(response);
+    
+  } catch (error) {
+    return internalServerError(res);
+  }
+};
+
+// UPDATE
+export const updateBook = async (req, res) => {
+  try {
+    const fileData = req.file
+    const { error } = joi.object({bid}).validate({bid: req.body.bid})
+    if (error) {
+      if (fileData) cloudinary.uploader.destroy(fileData.filename)
+      return badRequest(error.details[0].message, res)
+    }
+
+    const response = await services.updateBook(req.body, fileData);
+    return res.status(200).json(response);
+    
+  } catch (error) {
+    return internalServerError(res);
+  }
+};
+
+// DELETE
+export const deleteBook = async (req, res) => {
+  try {
+    const { error } = joi.object({bids}).validate(req.query)
+    if (error) {
+      return badRequest(error.details[0].message, res)
+    }
+
+    const response = await services.deleteBook(req.query.bids);
     return res.status(200).json(response);
     
   } catch (error) {
--- src/services/book.js
+++ src/services/book.js
@@ -1,8 +1,7 @@
 import db from "../models";
 import { Op } from "sequelize";
-import {v4 as generateId} from 'uuid'
-const cloudinary = require('cloudinary').v2
-
+import { v4 as generateId } from "uuid";
+const cloudinary = require("cloudinary").v2;
 
 // READ
 export const getBooks = ({ page, limit, order, name, available, ...query }) =>
@@ -21,7 +20,7 @@ export const getBooks = ({ page, limit, order, name, available, ...query }) =>
         where: query,
         ...queries,
         attributes: {
-          exclude: ["category_code"],
+          exclude: ["category_code", "description"],
         },
         include: [
           {
@@ -48,19 +47,57 @@ export const createNewBook = (body, fileData) => new Promise(async (resolve, rej
       const response = await db.Book.findOrCreate({
         where: { title: body.title },
         defaults: {
-            ...body,
-            id: generateId(),
-            image: fileData?.path
-        }
+          ...body,
+          id: generateId(),
+          image: fileData?.path,
+        },
       });
 
       resolve({
         err: response[1] ? 0 : 1,
         mes: response[1] ? "Created" : "Can't create new book",
       });
-      if (fileData && !response[1]) cloudinary.uploader.destroy(fileData.filename)
+      if (fileData && !response[1])
+        cloudinary.uploader.destroy(fileData.filename);
     } catch (error) {
       reject(error);
-      if (fileData) cloudinary.uploader.destroy(fileData.filename)
+      if (fileData) cloudinary.uploader.destroy(fileData.filename);
     }
   });
+
+// UPDATE
+export const updateBook = ({bid, ...body}, fileData) => new Promise(async (resolve, reject) => {
+    try {
+      if ( fileData ) body.image = fileData?.path
+      const response = await db.Book.update(body, {
+        where: {id: bid}
+      });
+
+      resolve({
+        err: response[0] > 0 ? 0 : 1,
+        mes: response[0] > 0 ? `${response[0]} book updated` : "Can't update new book/ book id not found",
+      });
+      if (fileData && response[0] === 0) cloudinary.uploader.destroy(fileData.filename);
+
+    } catch (error) {
+      reject(error);
+      if (fileData) cloudinary.uploader.destroy(fileData.filename);
+    }
+  });
+
+  // DELETE
+export const deleteBook = (bids) => new Promise(async (resolve, reject) => {
+  try {
+    const response = await db.Book.destroy({
+      where: {id: bids}
+    });
+
+    resolve({
+      err: response > 0 ? 0 : 1,
+      mes: `${response} book deleted`,
+    });
+
+  } catch (error) {
+    reject(error);
+  }
+});
